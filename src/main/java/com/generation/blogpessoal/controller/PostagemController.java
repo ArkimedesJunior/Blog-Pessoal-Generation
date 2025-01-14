14089d003e28e0443b80f607df0c2fc35dcf362c
ArkimedesJunior <arkimedesjunior01@gmail.com>
2025-01-14 16:30:28 -0300

    Atividade do dia 14/01

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -20,74 +20,78 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
-import jakarta.validation.Valid; 
+import jakarta.validation.Valid;
 
-
-
-@RestController   // Define ao spring que essa classe é uma controller
-@RequestMapping("/postagens") // Define qual endpoint vai ser tratado por essa classe
-@CrossOrigin(origins  = "*", allowedHeaders = "*")  // Libera o acesso a qualquer front
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
-
-	@Autowired // O spring dá autonomia para a Interface poder invocar os metodos
+	
+	@Autowired
 	private PostagemRepository postagemRepository;
 	
-	@GetMapping // Indica que essa método é chamado em verbos/Metodos HTTP do tipo Get
+	@Autowired
+	private TemaRepository temaRepository;
+	
+	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll(){
-		return ResponseEntity.ok(postagemRepository.findAll()); // SELECT * FROM tb_postagens
+		return ResponseEntity.ok(postagemRepository.findAll());
 	}
-		@GetMapping("/{id}")
-		public ResponseEntity<Postagem> getById(@PathVariable Long id) {
-			return postagemRepository.findById(id)
-					.map(resposta -> ResponseEntity.ok(resposta))
-					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-			
-			
-	/*Essa anotação é usada para mapear uma requisição HTTP GET para o método getById.
-	O /{id} significa que o valor de id será extraído da URL da requisição. 
-	Por exemplo, se a URL da requisição for /postagens/5, o valor 5 será atribuído ao parâmetro id.
-			
-		O método getById será executado quando uma requisição GET for feita à URL /postagens/{id}.
-O parâmetro @PathVariable Long id indica que o valor do parâmetro id na URL será passado para o método.
- No exemplo mencionado, se a URL for /postagens/5, o valor id será 5.	
-			*/
-			
-		
-		}
-		
-		@GetMapping("/titulo/{titulo}")
-		public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
-			return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
-		}
-		
-		@PostMapping
-		public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
-			return ResponseEntity
-					.status(HttpStatus.CREATED)
-					.body(postagemRepository.save(postagem));
-		}
-		
-		@PutMapping
-		public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
-			return postagemRepository.findById(postagem.getId())
-					.map(resposta -> ResponseEntity.status(HttpStatus.OK)
-							.body(postagemRepository.save(postagem)))
-					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-		}
-		
-		@ResponseStatus(HttpStatus.NO_CONTENT)
-		@DeleteMapping("/{id}")
-		public void delete(@PathVariable Long id ) {
-			Optional<Postagem> postagem = postagemRepository.findById(id);
-		
-			if(postagem.isEmpty ())
-				throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-			
-			postagemRepository.deleteById(id);
-		}		
 
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){
+		return postagemRepository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+		if (temaRepository.existsById(postagem.getTema().getId()))
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(postagemRepository.save(postagem));
+			
+	   throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+		if (postagemRepository.existsById(postagem.getId())) {
+			
+			if (temaRepository.existsById(postagem.getTema().getId()))
+				return ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem));
+						
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
+			
+		}
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+					
+		
+		
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Postagem> postagem = postagemRepository.findById(id);
+		
+		if(postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		postagemRepository.deleteById(id);				
+	}
+	
 }
+
 	
 
 
--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,58 +4,82 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.generation.blogpessoal.controller.Tema;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-@Entity //  tratar ela como uma tabela
-@Table(name = "tb_postagens") // indica o nome da tabela no banco de dados
+@Entity
+@Table(name = "tb_postagens")
 public class Postagem {
-	
-	
-    @Id // Ela é a chave primaria (PRIMARY KEY)
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto incremento
+    
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private Long id;
-    
-    @NotBlank(message = "Esse Campo é Obrigatorio") // Titulo da postagem
-    @Size(min = 5, max = 100, message = "Digite no minimo 5 e no maximo 100 caracteres")
-    private String titulo;
-    
-    @NotBlank(message = "Esse Campo é Obrigatorio") // Titulo da postagem
-    @Size(min = 10, max = 1000, message = "Digite no minimo 10 e no maximo 1000 caracteres")
+	
+	@NotBlank(message = "O atributo título é Obrigatório!") 
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotBlank(message = "O atributo texto é Obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
 	private String texto;
 	
 	@UpdateTimestamp
-	private LocalDateTime data; // 10/1025   18:25:58
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getTitulo() {
-		return titulo;
-	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getTexto() {
-		return texto;
-	}
-	public void setTexto(String texto) {
-		this.texto = texto;
-	}
-	public LocalDateTime getData() {
-		return data;
-	}
-	public void setData(LocalDateTime data) {
-		this.data = data;
-	}
-	
-	
+	private LocalDateTime data;
+
+    @ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return this.texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public LocalDateTime getData() {
+        return this.data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+  
+    public Tema getTema() {
+        return this.tema;
+    }
+
+    public void setTema(Tema tema) {
+        this.tema = tema;
+    }
+
+    
+    
+    
 }
